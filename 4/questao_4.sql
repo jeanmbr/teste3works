7fdc167b3a3de421d35012f891a179757844e69e
Jean Machado <jeanmachadobrasil@gmail.com>
2025-09-09 19:16:51 -0300

    Correção na questão 4

--- 4/questao_4.sql
+++ 4/questao_4.sql
@@ -1,13 +1,6 @@
 CREATE VIEW vendas_diarias AS
-SELECT 
-    vendas.data_venda,
-    produtos.nome_produto,
-    SUM(produtos.preco * vendas.quantidade) AS faturamento_total
-FROM 
-    vendas
-JOIN 
-    produtos ON vendas.produto_id = produtos.id
-GROUP BY 
-    vendas.data_venda, produtos.nome_produto
-ORDER BY 
-    vendas.data_venda DESC, faturamento_total DESC;
+SELECT vendas.data_venda, produtos.nome_produto, SUM(produtos.preco * vendas.quantidade) AS faturamento_total
+FROM vendas
+JOIN produtos ON vendas.produto_id = produtos.id
+GROUP BY vendas.data_venda, produtos.nome_produto
+ORDER BY vendas.data_venda DESC, faturamento_total DESC;
